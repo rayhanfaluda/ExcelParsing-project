1a82f0c2d4dffbbeea1bb2f35d59335162c18a75
Rayhan Faluda <rayhanfaluda@gmail.com>
2022-04-25 13:08:53 +0700

    Add API for Download to Excel

--- src/main/java/com/mandiri/controller/ReportController.java
+++ src/main/java/com/mandiri/controller/ReportController.java
@@ -5,7 +5,11 @@ import com.mandiri.entity.Report;
 import com.mandiri.helper.ExcelHelper;
 import com.mandiri.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,13 +28,13 @@ public class ReportController {
     ReportService reportService;
 
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> exportExcel(@RequestParam(name = "file") MultipartFile file) throws IOException {
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
         String message = "";
-        Path tempDir = Files.createTempDirectory("");
-        File tempFile = tempDir.resolve(file.getOriginalFilename()).toFile();
+
         if (ExcelHelper.hasExcelFormat(file)) {
             try {
-                reportService.save(tempFile);
+                reportService.save(file);
+
                 message = "Uploaded the file successfully: " + file.getOriginalFilename();
                 return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
             } catch (Exception e) {
@@ -43,9 +47,9 @@ public class ReportController {
     }
 
     @GetMapping("/reports")
-    public ResponseEntity<List<Report>> getAllTutorials() {
+    public ResponseEntity<List<Report>> getAllReports() {
         try {
-            List<Report> reports = reportService.getAllTutorials();
+            List<Report> reports = reportService.getAllReports();
             if (reports.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -55,4 +59,14 @@ public class ReportController {
         }
     }
 
+    @GetMapping("/download")
+    public ResponseEntity<Resource> getFile() {
+        String filename = "reports.xlsx";
+        InputStreamResource file = new InputStreamResource(reportService.load());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                .body(file);
+    }
 }
--- src/main/java/com/mandiri/helper/ExcelHelper.java
+++ src/main/java/com/mandiri/helper/ExcelHelper.java
@@ -1,8 +1,6 @@
 package com.mandiri.helper;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,53 +17,102 @@ public class ExcelHelper {
     public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
     static String[] HEADERs = { "fullName", "birthDate", "birthPlace", "address", "phoneNumber", "gender" };
     static String SHEET = "Report";
+
     public static boolean hasExcelFormat(MultipartFile file) {
         if (!TYPE.equals(file.getContentType())) {
             return false;
         }
         return true;
     }
-    public static List<Report> excelToReport(File file) {
+
+    public static ByteArrayInputStream reportsToExcel(List<Report> reports) {
+
+        try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+            Sheet sheet = workbook.createSheet(SHEET);
+
+            // Header
+            Row headerRow = sheet.createRow(0);
+
+            for (int col = 0; col < HEADERs.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellValue(HEADERs[col]);
+            }
+
+            int rowIdx = 1;
+            for (Report report : reports) {
+                Row row = sheet.createRow(rowIdx++);
+
+                row.createCell(0).setCellValue(report.getId());
+                row.createCell(1).setCellValue(report.getFullName());
+                row.createCell(2).setCellValue(report.getBirthDate());
+                row.createCell(3).setCellValue(report.getBirthPlace());
+                row.createCell(4).setCellValue(report.getAddress());
+                row.createCell(5).setCellValue(report.getPhoneNumber());
+            }
+
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
+        }
+    }
+
+    public static List<Report> excelToReports(InputStream is) {
         try {
-            Workbook workbook = WorkbookFactory.create(file);
+            Workbook workbook = new XSSFWorkbook(is);
+
             Sheet sheet = workbook.getSheet(SHEET);
             Iterator<Row> rows = sheet.iterator();
+
             List<Report> reports = new ArrayList<Report>();
+
             int rowNumber = 0;
             while (rows.hasNext()) {
                 Row currentRow = rows.next();
+
                 // skip header
                 if (rowNumber == 0) {
                     rowNumber++;
                     continue;
                 }
+
                 Iterator<Cell> cellsInRow = currentRow.iterator();
+
                 Report report = new Report();
+
                 int cellIdx = 0;
                 while (cellsInRow.hasNext()) {
                     Cell currentCell = cellsInRow.next();
+
                     switch (cellIdx) {
                         case 0:
                             report.setFullName(currentCell.getStringCellValue());
                             break;
+
                         case 1:
                             report.setBirthDate(currentCell.getDateCellValue());
                             break;
+
                         case 2:
                             report.setBirthPlace(currentCell.getStringCellValue());
                             break;
+
                         case 3:
                             report.setAddress(currentCell.getStringCellValue());
                             break;
+
                         case 4:
                             report.setPhoneNumber(currentCell.getStringCellValue());
                             break;
+
                         case 5:
                             report.setGender(currentCell.getStringCellValue());
                             break;
+
                         default:
                             break;
                     }
+
                     cellIdx++;
                 }
                 reports.add(report);
