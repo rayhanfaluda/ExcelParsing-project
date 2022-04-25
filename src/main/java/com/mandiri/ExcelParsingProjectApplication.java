0ad416be310d34b6c0a1fc058b5e34700770bc61
Rayhan Faluda <rayhanfaluda@gmail.com>
2022-04-25 08:12:18 +0700

    Initial commit

---
+++ src/main/java/com/mandiri/ExcelParsingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mandiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelParsingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelParsingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandiri/ExcelParsingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelParsingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
